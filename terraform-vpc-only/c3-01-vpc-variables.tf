2694d0cdae8eedfbe680c85e95f88861b8a30a16
kewang888 <ke.wang01@yahooinc.com>
2025-03-06 21:22:21 +0000

    check in for today

---
+++ terraform-vpc-only/c3-01-vpc-variables.tf
@@ -0,0 +1,43 @@
+# VPC Input Variables
+
+# VPC CIDR Block
+variable "vpc_cidr_block" {
+  description = "VPC CIDR Block"
+  type        = string
+  default     = "10.59.0.0/16"
+}
+
+# VPC Availability Zones
+variable "vpc_availability_zones" {
+  description = "VPC Availability Zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+# VPC Public Subnets
+variable "vpc_public_subnets" {
+  description = "VPC Public Subnets"
+  type        = list(string)
+  default     = ["10.59.0.0/18", "10.59.64.0/18"]
+}
+
+# VPC Private Subnets
+variable "vpc_private_subnets" {
+  description = "VPC Private Subnets"
+  type        = list(string)
+  default     = ["10.59.128.0/18", "10.59.192.0/18"]
+}
+
+# VPC Enable NAT Gateway (True or False)
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT Gateways for Private Subnets Outbound Communication"
+  type        = bool
+  default     = false
+}
+
+# VPC Single NAT Gateway (True or False)
+variable "vpc_single_nat_gateway" {
+  description = "Enable only single NAT Gateway in one Availability Zone to save costs during our demos"
+  type        = bool
+  default     = true
+}
